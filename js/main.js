47b9ecaf34e37e457c109e1209a44eabcce526c6
Natalia-Ushakova <n.n.ushakova@gmail.com>
2018-11-06 10:38:20 -0500

    alt attribute modified

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = "restaurant";
+  image.alt = "Photo of ${restaurant.name} restaurant";
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
